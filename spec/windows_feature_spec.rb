061949a40e2b52812202c7202d3fe3dd70fd131a
miguelaferreira <miguelferreira@me.com>
2014-12-16 21:23:42 +0100

    Fix windows feature spec error (by setting attribute in chef run)

--- spec/windows_feature_spec.rb
+++ spec/windows_feature_spec.rb
@@ -2,7 +2,11 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 describe 'chefspec_windows_tests::windows_feature' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2').converge(described_recipe) }
+  let(:chef_run) do 
+    ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2') do |node|
+      node.set['windows']['feature_provider'] = 'powershell'
+    end.converge(described_recipe)
+  end
   
   it 'installs a windows feature' do
     expect(chef_run).to install_windows_feature('NET-Framework-Core')
